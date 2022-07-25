7c67ce93f4b88e94893d2cc9b9fb4a7a07bc976a
Sara Blom <sara.cecilia.blom@gmail.com>
2022-07-25 07:54:47 +0200

    add footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -11,8 +11,30 @@
         <slot></slot>
     </main>
     <footer>
-        Copyright 2022 Sara Blom
-    </footer>
+        <div class="copy">
+          <p>&#169; Sara Blom, 2022</p>
+        </div>
+        <div class="social-media">
+          <a href="https://github.com/sarablom/" target="”_blank”"
+            ><img src="img/github-sign@2x.png" alt="Github-link"
+          /></a>
+          <a
+            href="https://www.linkedin.com/in/sara-blom-8ba389144/"
+            target="”_blank”"
+            ><img src="img/linkedin-logo@2x.png" alt="LinkedIn-link"
+          /></a>
+          <a
+            href="https://www.instagram.com/sarafox.barnklader/"
+            target="”_blank”"
+            ><img src="img/instagram@2x.png" alt="Instagram-link"
+          /></a>
+          <a
+            href="https://open.spotify.com/artist/1Vhr0X4FgDzqk8s33DUWCz?si=_oi5Cq8mSNuOgx3o4DN0HQ"
+            target="”_blank”"
+            ><img src="img/spotify@2x.png" alt="Spotify-link"
+          /></a>
+        </div>
+      </footer>
 </div>
 
 <style>

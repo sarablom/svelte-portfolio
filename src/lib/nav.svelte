8e8288e805c9c7948014d65a9f6f0594ccf55441
Sara Blom <sara.cecilia.blom@gmail.com>
2022-07-25 08:25:32 +0200

    add link to home page

--- src/lib/nav.svelte
+++ src/lib/nav.svelte
@@ -1,5 +1,6 @@
 <nav>
     <ol>
+        <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
         <li><a href="/projects">Projects</a></li>
         <li><a href="/experience">Experience</a></li>

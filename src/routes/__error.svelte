9ff825416ffe0b7d15e109f9367f59ec065c5c4d
Sara Blom <sara.cecilia.blom@gmail.com>
2022-07-25 18:20:58 +0200

    add error fallback page

---
+++ src/routes/__error.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<section>
+    <h1>404</h1>
+
+    <p>Ooops, this page doesn't seem to exist, please return to Home page!</p>
+    <a href="/">Home</a>
+</section>
+
+<style>
+
+</style>

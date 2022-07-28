5fe5702797f3a3a839d13af8235564bca80749d6
Sara Blom <sara.cecilia.blom@gmail.com>
2022-07-28 17:52:33 +0200

    create footer component

---
+++ src/lib/footer.svelte
@@ -0,0 +1,41 @@
+<script>
+        import { GithubIcon, InstagramIcon, LinkedinIcon } from "svelte-feather-icons";
+</script>
+
+
+<footer>
+<div class="copy">
+    <p>&#169; Sara Blom, 2022</p>
+</div>
+<div class="social-media">
+    <a href="https://github.com/sarablom/" target="”_blank”"
+    ><GithubIcon size="3x" /></a>
+    <a
+    href="https://www.linkedin.com/in/sara-blom-8ba389144/"
+    target="”_blank”"
+    ><LinkedinIcon size="3x" /></a>
+    <a
+    href="https://www.instagram.com/sarafox.barnklader/"
+    target="”_blank”"
+    ><InstagramIcon size="3x" /></a>
+    <a
+    href="https://open.spotify.com/artist/1Vhr0X4FgDzqk8s33DUWCz?si=_oi5Cq8mSNuOgx3o4DN0HQ"
+    target="”_blank”"
+    ><img src="img/spotify@2x.png" alt="Spotify-link"
+    /></a>
+</div>
+</footer>
+
+<style>
+    footer {
+        min-height: 3rem;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 32px;
+    }
+
+    .social-media {
+        display: flex;
+    }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,8 @@
 <script>
-    import Nav from "../lib/nav.svelte";
+    import Footer from "$lib/footer.svelte";
+    import Nav from "$lib/nav.svelte";
     import "../styles/globalStyles.css";
-    import { GithubIcon, InstagramIcon, LinkedinIcon } from "svelte-feather-icons";
+
 
 </script>
 
@@ -12,28 +13,7 @@
     <main>
         <slot></slot>
     </main>
-    <footer>
-        <div class="copy">
-          <p>&#169; Sara Blom, 2022</p>
-        </div>
-        <div class="social-media">
-          <a href="https://github.com/sarablom/" target="”_blank”"
-            ><GithubIcon size="3x" /></a>
-          <a
-            href="https://www.linkedin.com/in/sara-blom-8ba389144/"
-            target="”_blank”"
-            ><LinkedinIcon size="3x" /></a>
-          <a
-            href="https://www.instagram.com/sarafox.barnklader/"
-            target="”_blank”"
-            ><InstagramIcon size="3x" /></a>
-          <a
-            href="https://open.spotify.com/artist/1Vhr0X4FgDzqk8s33DUWCz?si=_oi5Cq8mSNuOgx3o4DN0HQ"
-            target="”_blank”"
-            ><img src="img/spotify@2x.png" alt="Spotify-link"
-          /></a>
-        </div>
-      </footer>
+    <Footer />
 </div>
 
 <style>
@@ -57,17 +37,5 @@
         
       max-width: 1000px;
  
-    }
-
-    footer {
-        min-height: 3rem;
-        display: flex;
-        align-items: center;
-        justify-content: space-between;
-        padding: 32px;
-    }
-
-    .social-media {
-        display: flex;
     }
 </style>

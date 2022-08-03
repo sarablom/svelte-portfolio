1e366e45e3161731707b3c142a892e0af21e25fe
Sara Blom <sara.cecilia.blom@gmail.com>
2022-08-03 22:11:45 +0200

    add pic and component to start page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,46 @@
 <script>
 import MainHeading from "$lib/mainHeading.svelte";
+import StarConstellation from "$lib/starConstellation.svelte";
+import memoji from "$lib/assets/memoji.jpg";
 
 </script>
 
 
 <MainHeading headingTitle="Sara Blom - software developer" />
-<p></p>
+<div class="wrapper">
+    <StarConstellation />
+    <img src={memoji} alt="Avatar">
+</div>
+
+<style>
+    .wrapper {
+        display: flex;
+        gap: 32px;
+    }
+
+    img {
+        width: 300px;
+        border-radius: 50%;
+    }
+
+    @media screen and (max-width: 800px) {        
+        img {
+        width: 200px;  
+        height: 200px;
+        }
+    }
+
+    @media screen and (max-width: 650px) {
+        .wrapper {
+        flex-direction: column-reverse;
+        align-items: center;
+        }
+
+        img {
+        width: 300px; 
+        height: 300px; 
+        }
+    }
+
+
+</style>

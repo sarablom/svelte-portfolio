12163c6064cede6c8b3faa0b7c8f80c828975cb5
Sara Blom <sara.cecilia.blom@gmail.com>
2022-08-05 07:55:04 +0200

    style button

--- src/lib/button.svelte
+++ src/lib/button.svelte
@@ -1,19 +1,28 @@
 <script lang="ts">
     export let buttonTitle: string;
+    
 </script>
 
-<button on:click>{buttonTitle}</button>
+<button class="button" on:click>{buttonTitle}</button>
 
-<style lng="scss">
+<style lang="scss">
     button {
+        align-self: center;
         background-color: var(--color-aubergine);
+        background: linear-gradient(90deg, rgba(139,71,93,1) 23%, rgba(148,98,143,1) 100%);
         color: var(--color-peachpuff);
         border: none;
-        margin: 8px 0;
-        padding: 8px 16px;
+        margin: 32px 0;
+        padding: 16px 32px;
         border-radius: 4px;
-        font-size: 16px;
+        font-size: var(--font-size-m);
         font-weight: 600;
         cursor: pointer;
+        box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
+
+        &:hover {
+            /* background: linear-gradient(90deg, rgba(148,98,143,1) 23%, rgba(139,71,93,1) 100%); */
+            transform: scale(1.02);
+        }
     }
 </style>

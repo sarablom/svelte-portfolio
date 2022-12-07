357fa178353acb4909a9fe8694b96cc27e998f26
Sara Blom <sara.cecilia.blom@gmail.com>
2022-12-07 09:52:33 +0100

    add screen reader texts to social media links

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,5 +1,6 @@
 <script>
-        import { GithubIcon, InstagramIcon, LinkedinIcon } from "svelte-feather-icons";
+    import { GithubIcon, InstagramIcon, LinkedinIcon } from "svelte-feather-icons";
+	import ScreenReaderText from "./screenReaderText.svelte";
 </script>
 
 
@@ -9,16 +10,27 @@
             <p>&#169; Sara Blom, 2022</p>
         </div>
         <div class="social-media">
-            <a href="https://github.com/sarablom/" target="”_blank”"
-            ><GithubIcon size="3x" /></a>
+            <a 
+                href="https://github.com/sarablom/" 
+                target="_blank”"
+            >
+                <GithubIcon size="3x" />
+                <ScreenReaderText text="GitHub" />
+            </a>
             <a
-            href="https://www.linkedin.com/in/sara-blom-8ba389144/"
-            target="”_blank”"
-            ><LinkedinIcon size="3x" /></a>
+                href="https://www.linkedin.com/in/sara-blom-8ba389144/"
+                target="_blank”"
+            >
+                <LinkedinIcon size="3x" />
+                <ScreenReaderText text="LinkedIn" />
+            </a>
             <a
-            href="https://www.instagram.com/sarafox.barnklader/"
-            target="”_blank”"
-            ><InstagramIcon size="3x" /></a>
+                href="https://www.instagram.com/sarafox.barnklader/"
+                target="_blank”"
+            >
+                <InstagramIcon size="3x" />
+                <ScreenReaderText text="Instagram" />
+            </a>
         </div>
     </div>
 </footer>
---
+++ src/lib/screenReaderText.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  export let text: string;
+</script>
+
+<span>{text}</span>
+
+<style lang="scss">
+span {
+  border: none;
+  clip: rect(0, 0, 0, 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+}
+</style>

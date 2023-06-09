077530fc0a10c8428a4c473aaf6c3a44aedad20d
Sara Blom <sara.cecilia.blom@gmail.com>
2023-06-09 09:38:52 +0200

    create gradient on main heading

--- src/lib/mainHeading.svelte
+++ src/lib/mainHeading.svelte
@@ -1,13 +1,24 @@
 <script lang="ts">
-    export let headingTitle: string;
+  export let headingTitle: string;
 </script>
 
 <h1>{headingTitle}</h1>
 
 <style lng="scss">
-        h1 {
-        text-align: center;
-        margin-bottom: 32px;
-        font-size: var(--font-size-xxl);
-    }
+	h1 {
+		text-align: center;
+		margin-bottom: 32px;
+		font-size: var(--font-size-xxl);
+		background: -webkit-linear-gradient(
+			0deg, 
+			var(--color-dark-peachpuff),
+			var(--color-peachpuff),
+			var(--color-light-peachpuff),  
+			var(--color-peachpuff),
+			var(--color-dark-peachpuff)
+		);
+		-webkit-background-clip: text;
+		background-clip: text;
+		-webkit-text-fill-color: transparent;
+	}
 </style>

e1ef18f4346d909c4dc4902e16e9c0a6944f83f7
sara.blom <sara.blom@medtankengroup.se>
2023-08-08 18:11:45 +0200

    update back button

--- src/routes/projects/[id].svelte
+++ src/routes/projects/[id].svelte
@@ -1,101 +1,113 @@
 <script context="module" lang="ts">
-  import type { Load } from '@sveltejs/kit';
-  import type { Project } from 'src/models/project';
-  
-  export const load: Load = async ({ params, fetch }) => {
-    const id = params.id;
-      const response = await fetch("../projects.json");
-      const projects = await response.json();
+	import type { Load } from "@sveltejs/kit";
+	import type { Project } from "src/models/project";
 
-      const project = projects.find((project: Project) => {
-        return project.id.toString() === id;
-      })
-      
-      
-      if (response.ok) {
-          return {
-            props: {
-              project: project as Project
-            }
-          }
-        }
-        
-        return {
-          status: response.status,
-          error: new Error("Could not fetch the project")
-        }
-      };
-      
-    
+	export const load: Load = async ({ params, fetch }) => {
+		const id = params.id;
+		const response = await fetch("../projects.json");
+		const projects = await response.json();
+
+		const project = projects.find((project: Project) => {
+			return project.id.toString() === id;
+		});
+
+		if (response.ok) {
+			return {
+				props: {
+					project: project as Project,
+				},
+			};
+		}
+
+		return {
+			status: response.status,
+			error: new Error("Could not fetch the project"),
+		};
+	};
 </script>
-<script  lang="ts">
-  import MainHeading from '$lib/mainHeading.svelte';
-  export let project: Project; 
-  export let { techniques } = project;
 
+<script lang="ts">
+	import {
+		AirplayIcon,
+		AtSignIcon,
+		ArrowLeftCircleIcon,
+	} from "svelte-feather-icons";
+	import MainHeading from "$lib/mainHeading.svelte";
+	export let project: Project;
+	export let { techniques } = project;
 </script>
 
 <section class="project">
-  <MainHeading headingTitle={project.title} />
-  <a class="back-button" href="/projects">Tillbaka</a>
-  <p>{project.description}</p>
-  <h3>Techniques</h3>
-  <ol>
-    {#each techniques as technique}
-      <li>{technique}</li>
-    {/each}
-  </ol>
-  <a
-      class="project-link"
-      href={project.pageHref}
-      target="_blank"
-      rel="noopener noreferrer"
-  >
-    The site
-  </a>
-  <span class="dot white"></span>
-  {#if (project.codeHref)}
-    <a
-    class="project-link"
-    href={project.codeHref}
-    target="_blank"
-    rel="noopener noreferrer"
-    >The code
-    </a>
-  {:else}
-    <span class="no-code">Code not public</span>
-  {/if}
+	<div class="header-wrapper">
+		<a class="back-button" href="/projects">
+			<ArrowLeftCircleIcon size="36" />
+		</a>
+		<MainHeading headingTitle={project.title} />
+	</div>
+	<p>{project.description}</p>
+	<h3>Techniques</h3>
+	<ol>
+		{#each techniques as technique}
+			<li>{technique}</li>
+		{/each}
+	</ol>
+	<a
+		class="project-link"
+		href={project.pageHref}
+		target="_blank"
+		rel="noopener noreferrer"
+	>
+		The site
+	</a>
+	<span class="dot white" />
+	{#if project.codeHref}
+		<a
+			class="project-link"
+			href={project.codeHref}
+			target="_blank"
+			rel="noopener noreferrer"
+			>The code
+		</a>
+	{:else}
+		<span class="no-code">Code not public</span>
+	{/if}
 </section>
 
 <style lang="scss">
-  p {
-    padding: 32px 0;
-  }
-  ol {
-    list-style-type: none;
-    padding-left: 0;
-    padding-bottom: 32px;
-  }
+	.header-wrapper {
+		position: relative;
+	}
 
-  span.dot {
-      display: inline-block;
-      width: 8px;
-      height: 8px;
-      border-radius: 50%;
-      background-color: var(--color-dark-peachpuff);
-  }
+	p {
+		padding: 32px 0;
+	}
+	ol {
+		list-style-type: none;
+		padding-left: 0;
+		padding-bottom: 32px;
+	}
 
-  a.back-button {
-    border: 1px solid var(--color-dark-peachpuff);
-    border-radius: 4px;
-    padding: 4px 8px;
-  }
+	span.dot {
+		display: inline-block;
+		width: 8px;
+		height: 8px;
+		border-radius: 50%;
+		background-color: var(--color-dark-peachpuff);
+	}
 
-  a.project-link {
-      padding: 4px 2px;
+	a.back-button {
+		position: absolute;
+		top: 12px;
+		left: 0;
+		border: none;
+		margin-right: auto;
+	}
 
-  &:focus {
-      outline: 1px solid var(--color-aubergine);
-  }
-}  
+	a.project-link {
+		padding: 4px 2px;
+
+		&:focus {
+			outline: 1px solid var(--color-aubergine);
+		}
+	}
 </style>

b1448a027ba224847221a799184155552f415874
sara.blom <sara.blom@medtankengroup.se>
2023-08-08 17:36:15 +0200

    update margin of main heading

--- src/lib/mainHeading.svelte
+++ src/lib/mainHeading.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  export let headingTitle: string;
+	export let headingTitle: string;
 </script>
 
 <h1>{headingTitle}</h1>
@@ -7,16 +7,21 @@
 <style lng="scss">
 	h1 {
 		text-align: center;
-		margin-bottom: 32px;
+		margin-bottom: 16px;
 		font-size: var(--font-size-xxl);
 		background-image: linear-gradient(
-            to right, 
-            var(--color-aubergine), 
-            var(--color-dark-peachpuff) 50%,
-            var(--color-dark-peachpuff) 50%
-            );
+			to right,
+			var(--color-aubergine),
+			var(--color-dark-peachpuff) 50%,
+			var(--color-dark-peachpuff) 50%
+		);
 		-webkit-background-clip: text;
 		background-clip: text;
 		-webkit-text-fill-color: transparent;
+	}
+	@media screen and (max-width: 801px) {
+		h1 {
+			margin-bottom: 0;
+		}
 	}
 </style>
